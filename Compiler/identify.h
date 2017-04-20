53a1fa1c523b0a0446f8553e9b6946470b401164
= <=>
2017-04-19 18:36:19 -0700

    CompilerV.1.0 Finished the first part of Compiler that deals with getting rid of extra whitespace and comment lines. TODO: Add grammer of compiler langague, check for errors Translate txt to cpp Compile cpp

---
+++ Compiler/identify.h
@@ -0,0 +1,149 @@
+// Author: Andrew Le
+// Identifier class
+// Takes out extra white space/comment lines/blank lines then writes the new code in txt file
+
+
+#ifndef IDENTIFY_h
+#define IDENTIFY_h
+#include <fstream>
+
+using namespace std;
+
+class identify
+{
+private:
+    
+    string words[200]; // can store a program that is 200 lines
+    string special[9] = {"=","*","-",";","(",")","+",",",":"}; // list of special characters
+    int size; // stores the size of the array
+    string reservedwords[5] = {"PROGRAM","BEGIN","END.","INTEGER","PRINT"}; // reserved words
+public:
+    
+    // default constructor
+    identify()
+    {
+        size = 0;
+    }
+    
+    
+    // Checks if the string is a reserved word
+    // INPUT: w- string we are checking
+    // OUTPUT: returns the length of the word using it as an index 
+    int isReserved(string w)
+    {
+        string word;
+        for (int i = 0; i < w.length(); i++)
+        {
+            word += w[i];
+            for(int j = 0; j < 5; j++)
+            {
+                if(word == reservedwords[j])
+                {
+                    words[size] = word;
+                    size++; //increment the size
+                    return (int)word.length();
+                }
+            }
+        }
+        return 0;
+    }
+    
+    
+    // checks if the string is a special char
+    // Input: string w
+    // Output: false/true
+    bool isSpecial(char w)
+    {
+        bool isSpecial = false;
+        for (int i = 0; i < 9; i++)
+        {
+            if (special[i].find(w) != special[i].npos) // checks to see if s is in special char list by comparing if it is found at a position that is not the nth position
+            {
+                isSpecial = true;
+            }
+        }
+        
+        return isSpecial;
+    }
+    
+    
+    // Checks string to see if there are comment lines and extra uneeded white space and removes both stores data into string array
+    // Input: string w - line from the file
+    // Output: None
+    void check(string w)
+    {
+        
+        int comment = 0; // stores the amt of // backslash lines if 2 then it is a comment line we will ignores
+        int i = isReserved(w); // checks if the string contains reserved word returns the legnth of reserved word and uses the length of that word as the starting point of i
+        while (i < w.length())
+        {
+            // if there is a comment found it will ignore rest of the line
+            if (comment == 2)
+            {
+                break;
+            }
+            else
+            {
+                if (w[i] == '/') // if a backspace key is a char
+                {
+                    comment++; // increment comment
+                    i++;
+                }
+                else if (isspace(w[i])) // if white space then increment
+                {
+                    i++;
+                }
+                else if (isSpecial(w[i])) // if char is a special char
+                {
+                    if(words[size].empty()) // if the current word is empty add on the special character
+                    {
+                        words[size] = w[i];
+                        size++;// increment the size
+                    }
+                    else // else the current word has items so add special char to next word and increment 2
+                    {
+                        words[size+1] = w[i];
+                        size += 2;
+                    }
+                    i++;
+                }
+                else // then the char is a letter or a digit so just add to existing words array
+                {
+                    words[size] = words[size] + w[i];
+                    i++;
+                }
+                
+            }
+        }
+    }
+    
+    
+    // prints out all the data properly formated into file
+    // Input: output ofstream to write to file
+    // Ouput: None
+    void printfile(ofstream &out)
+    {
+        int i = 0; // iterator
+        
+        while (i < size) // loop ends when i reachs the size
+        {
+            // if the word index is ; or BEGIN then we go to new line
+            if (words[i] == ";" || words[i] == "BEGIN")
+            {
+                out << words[i] << endl;
+            }
+            else
+            {
+                out << words[i] << " ";
+            }
+            i++; // increment
+        }
+        
+    }
+    
+
+    
+};
+
+
+#endif /* indentify_h */
---
+++ Compiler/main.cpp
@@ -0,0 +1,61 @@
+/*
+ 
+Author: Andrew Le
+Email: andrewle19@csu.fullerton.edu
+4/17/17
+ 
+Compiler that transforms a text file with code into a c++ executable code if it has no errors present
+ 
+ 
+ 
+*/
+
+#include <iostream>
+#include <string>
+#include "identify.h"
+#include <fstream>
+using namespace std;
+
+
+
+int main()
+{
+    
+    ifstream data; // handles opening and reading of the file data.txt
+    ofstream output; // handles oppening and writing of the file newdata.txt
+    string statement; // takes in lines from the data.txt
+    identify compiler; // intialize compiler
+    data.open("finalv1.txt"); // opens the code file
+    
+    
+    // checks if file successfully opened
+    if (data.is_open())
+    {
+        // takes in lines as long as the file is not at eof
+        while (data.eof() != true)
+        {
+            getline(data, statement); // gets the line
+            // checks for comment lines/extra white space removes all of them saves data to array in class
+            compiler.check(statement);
+        }
+      
+        // opens the finalv2.txt file or writes to it depending on if it exists
+        output.open("finalv2.txt");
+       
+        compiler.printfile(output); // prints all the new data to file
+        
+        cout << "Outputing to file" << endl;
+        output.close(); // close file
+    }
+    else
+    {
+        cout << "Could not open data.txt" << endl;
+    }
+    
+   
+    
+    
+    data.close(); // close the file
+    system("pause");
+    return 0;
+}
